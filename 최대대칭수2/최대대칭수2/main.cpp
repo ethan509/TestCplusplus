17e356d908316993bc3350ac1ccfe4df4f00e42c
ethan509 <ethanisyes@gmail.com>
2024-07-11 14:10:28 +0900

    한글깨짐 수정

--- 최대대칭수2/최대대칭수2/main.cpp
+++ 최대대칭수2/최대대칭수2/main.cpp
@@ -1,13 +1,14 @@
-#include <iostream>
+﻿#include <iostream>
 #include <map>
 #include <set>
 #include <algorithm>
 
 using namespace std;
 
+// 최대대칭수 -> 오류 해결 및 로직업그레이드.
 int solution(string str) {
 	int ret = 0;
-	map<char, int> map_;
+	map<char, int> map_;	// <int, int> 대신 <char, int>로 저장. more simple
 	set<char, greater<int>> set_;
 
 	for (const char ch : str) {
@@ -22,7 +23,7 @@ int solution(string str) {
 		}
 	}
 
-	char max = '0'-1;
+	char max = '0'-1; // 숫자 0보다 작은 ascii 값을 default value 로 설정
 	string answerStr = "";
 	answerStr.resize(str.size(), ' ');
 	int answerStrIdx = 0;
@@ -54,12 +55,13 @@ int solution(string str) {
 		}
 	}
 
+	// 한번도 max 값이 설정되지 않았을 때 추가되는 것을 방지하기 위해 if문 필요
 	if (max != '0' - 1) {
 		answerStr[idx] = max;
 	}
 
 	//cout << "answerStr: '" << answerStr << "'";
-
+	
 	int blankCnt = 0;
 	for (char ch : answerStr) {
 		if (ch == ' ')
